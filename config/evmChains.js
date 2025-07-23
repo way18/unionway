30bba5f285c23bb1852ff0a90019a891f6be6981
way18 <36079919+way18@users.noreply.github.com>
2025-07-23 13:37:47 +0700

    Create evmChains.js

---
+++ config/evmChains.js
@@ -0,0 +1,10 @@
+module.exports = {
+  sepolia: {
+    rpc: "https://sepolia.infura.io/v3/YOUR_INFURA_KEY",
+    address: "0xRECEIVER_ON_HOLESKY" // ganti dengan wallet tujuan
+  },
+  holesky: {
+    rpc: "https://ethereum-holesky.publicnode.com",
+    address: "0xRECEIVER_ON_SEPOLIA"
+  }
+};
