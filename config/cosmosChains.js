288c521e53117a58f0b68218ddbac3bfae099486
way18 <36079919+way18@users.noreply.github.com>
2025-07-23 13:38:29 +0700

    Create cosmosChains.js

---
+++ config/cosmosChains.js
@@ -0,0 +1,32 @@
+module.exports = {
+  sei: {
+    rpc: "https://sei-rpc.polkachu.com",
+    prefix: "sei",
+    denom: "usei",
+    address: "sei1receiverwallet..."
+  },
+  babylon: {
+    rpc: "https://rpc.babylonchain.io",
+    prefix: "bby",
+    denom: "ubbn",
+    address: "bby1receiverwallet..."
+  },
+  xion: {
+    rpc: "https://xion-rpc.polkachu.com",
+    prefix: "xion",
+    denom: "uxion",
+    address: "xion1receiverwallet..."
+  },
+  bsc: {
+    rpc: "https://bsc.publicnode.com",
+    prefix: "bnb",
+    denom: "ubnb",
+    address: "bnb1receiverwallet..." // bisa juga pakai alamat EVM
+  },
+  holesky: {
+    rpc: "https://ethereum-holesky.publicnode.com",
+    prefix: "eth",
+    denom: "wei",
+    address: "0xEVMReceiverOnHolesky"
+  }
+};
